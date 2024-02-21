6f1422ac28815119d04ad6ae3c9470e6fe37fe70
Юлия Тюрина <juliosoctober@gmail.com>
2024-02-21 16:03:52 +0300

    2 hw

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Banner } from '@/app/Components/Banner/Banner'
 import { Promo } from '@/app/Components/Promo/Promo'
-import { PopularCardsList } from "@/app/Components/CardsList/PopularCardsList";
-import { NewCardsList } from "./Components/CardsList/NewCardsList";
+import { CardsList } from "@/app/Components/CardsList/CardList";
+import { getGamesByCategory } from '@/app/data/data-utils';
 
 export default function Home() {
+  const popularGames = getGamesByCategory('popular');
+
+  const newGames = getGamesByCategory('new');
+
   return (
     <main className="main">
-      <Banner/>
-      <PopularCardsList/>
-      <NewCardsList/>
-      <Promo/>
+      <Banner />
+      <CardsList id={'popular'} title={'Популярное'} data={popularGames}>
+      </CardsList>
+      <CardsList id={'new'} title={'Новинки'} data={newGames}>
+      </CardsList>
+      <Promo />
     </main>
   );
 }
